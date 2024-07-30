cd356383254dd63bb1cb4c254fdd9cc5bcc71e7c
bhanuprakashsfdc <bhanusunny7@gmail.com>
2024-07-30 19:31:34 +0530

    Pushing blog posts

--- backend/data/blogData.js
+++ backend/data/blogData.js
@@ -1,11 +1,28 @@
 const blogData = [
     {
+      id: 1,
       slug: 'introduction-to-cloud-computing',
       title: 'Introduction to Cloud Computing',
+      description:"",
+      date: '2023-11-25',
       content: 'Detailed content...',
-      viewCount: 0
+      viewCount: 0,
+      tags: ["Sport", "Baseball", "Blue Jays"],
+      categories:["Finance"],
+      featureimage: "assets/img/main_post.jpeg",
     },
-    // Add more blog data here
+    {
+      id: 2,
+      slug: 'introduction-to-cloud-computing',
+      title: 'Introduction to Cloud Computing',
+      description:"",
+      date: '2023-11-25',
+      content: 'Detailed content...',
+      viewCount: 0,
+      tags: ["Sport", "Baseball", "Blue Jays"],
+      categories:["Finance"],
+      featureimage: "assets/img/main_post.jpeg",
+    }
   ];
   
   module.exports = blogData;
